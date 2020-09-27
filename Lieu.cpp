4f1c16293212f21cb1c17e9473a802b9d90378e0
TAIBI <sofiane.st98@gmail.com>
2020-09-27 20:36:49 +0200

    new function 'estAccessible'

--- Lieu.cpp
+++ Lieu.cpp
@@ -43,3 +43,49 @@ void Lieu::removeConnexion(connectionType_t mt, Lieu* l)
   if (mt==TRAIN)
     this->train[l->numero]=NULL;
 }
+
+bool Lieu::estAccessible(connectionType_t mt, const Lieu& l)
+{
+  if (mt==NONE)
+    return false;
+  if (mt==BATEAU)
+    return (this->bateau[l.numero]==&l);
+  if (mt==TRAIN)
+    return (this->train[l.numero]==&l);
+  if (mt==ALL)
+    return ((this->bateau[l.numero]==&l) || (this->train[l.numero]==&l));
+}
+/*
+long distance(connectionType_t mt, const Lieu& l)
+{
+  if (this->numero==l.numero)
+    return 0;
+  else if (this.estAccessible (mt, l))
+    return 1;
+  else
+    {
+      long dist[N]={ 0 };
+      int visited[N]={ 0 };
+      queue <string> file;
+      file.push(l1);
+      dist[index(l1)]=0;
+      visited[index(l1)]=1;
+      while (!file.empty())
+	{
+	  l1=file.front();
+	  file.pop();
+	  for (int i=0; i<N; i++)
+	    if(estAccessible(mt, l1, Villes[i]) && visited[i]==0)
+	      {
+		  file.push(Villes[i]);
+		  dist[i] = dist[index(l1)] + 1;
+		  visited[i]=1;
+	      }
+	}
+      if (dist[index(l2)]==0)
+	return -1;
+      else
+	return dist[index(l2)];
+    }
+}
+*/
--- Lieu.h
+++ Lieu.h
@@ -7,18 +7,20 @@ enum connectionType_t{NONE, TRAIN, BATEAU, ALL};
 class Lieu{
  private:
   std::string name;
-  long numero;
   long nbBateau;
   Lieu* bateau[];
   long nbTrain;
   Lieu* train[];
 
  public:
+  long numero;
   static long compteur;
   Lieu();
   Lieu(std::string);
   std::string getNom();
-  void addConnexion (connectionType_t, Lieu*);
-  void removeConnexion (connectionType_t, Lieu*);
+  void addConnexion(connectionType_t, Lieu*);
+  void removeConnexion(connectionType_t, Lieu*);
+  bool estAccessible(connectionType_t, const Lieu&);
+  long distance(connectionType_t, const Lieu&);
   ~Lieu();
 };
