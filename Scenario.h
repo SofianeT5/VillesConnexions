ab9f8ee20f4ff9b0342d3d3daadca69f2de4c0d4
TAIBI <sofiane.st98@gmail.com>
2020-10-06 00:19:14 +0200

    new function 'simulation()'

--- Scenario.h
+++ Scenario.h
@@ -17,10 +17,11 @@ class Scenario{
   ~Scenario();
   Personnage getPersonnage(std::string name);
   long getNbPers();
-  void initPersonnages();
+  void initPersonnages(std::string* noms_gangster, long nb_gangster, std::string* noms_policier, long nb_policier, std::string* noms_pigeon, long nb_pigeon);
   void initCarte();
-  void initScenario();
+  void initScenario(std::string* noms_gangster, long nb_gangster, std::string* noms_policier, long nb_policier, std::string* noms_pigeon, long nb_pigeon);
   void scene();
+  void simulation(std::string* noms_gangster, long nb_gangster, std::string* noms_policier, long nb_policier, std::string* noms_pigeon, long nb_pigeon, long nb_scene);
 };
 
 #endif
